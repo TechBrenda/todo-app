ff3df650e23ab12d85960ad1a91af806102e4efd
TechBrenda <tech.brenda@gmail.com>
2019-06-27 16:46:44 -0500

    Add state to counter

--- todo-app/src/components/counter/Counter.js
+++ todo-app/src/components/counter/Counter.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Counter.css';
 
-function Counter() {
-  return (
-  <div className='counter'>
-    <button onClick={increment}>+1</button>
-    <span className='count'>0</span>
-  </div>
-  );
-}
-
-function increment() {
-  console.log('increment');
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    this.increment = this.increment.bind(this);
+  }
+  
+  increment() {
+    this.setState((prevState) => {return {counter: prevState.counter + 1}});
+  }
+  
+  render() {
+    return (
+      <div className='counter'>
+        <button onClick={this.increment}>+1</button>
+        <span className='count'>{this.state.counter}</span>
+      </div>
+    );
+  }
 }
 
 export default Counter;
