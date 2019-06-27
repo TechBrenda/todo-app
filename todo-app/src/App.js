f5bc49065f9bf3b52cff118b3246f63f2f6acab1
TechBrenda <tech.brenda@gmail.com>
2019-06-27 14:13:55 -0500

    Reset for next example

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,6 +8,16 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className='LearningComponents'>
         My Hello World
         <FirstComponent />
         <SecondComponent />
