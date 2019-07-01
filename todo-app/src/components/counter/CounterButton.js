3312905ef26384f25c51f9443c585772398c1035
TechBrenda <tech.brenda@gmail.com>
2019-07-01 09:21:06 -0500

    Removed state from CounterButton

--- todo-app/src/components/counter/CounterButton.js
+++ todo-app/src/components/counter/CounterButton.js
@@ -3,17 +3,7 @@ import PropTypes from 'prop-types'
 import './Counter.css'
 
 class CounterButton extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      counter: 0
-    }
-  }
-
   increment = () => {
-    this.setState(prevState => {
-      return { counter: prevState.counter + this.props.by }
-    })
     this.props.incrementMethod(this.props.by)
   }
 
