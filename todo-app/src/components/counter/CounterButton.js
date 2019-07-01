4ad89b06f63573536059a7ddbcf3415e48980327
TechBrenda <tech.brenda@gmail.com>
2019-07-01 09:18:41 -0500

    Removed individual counters from buttons

--- todo-app/src/components/counter/CounterButton.js
+++ todo-app/src/components/counter/CounterButton.js
@@ -21,7 +21,6 @@ class CounterButton extends Component {
     return (
       <div className='counter'>
         <button onClick={this.increment}>+{this.props.by}</button>
-        <span className='count'>{this.state.counter}</span>
       </div>
     )
   }
