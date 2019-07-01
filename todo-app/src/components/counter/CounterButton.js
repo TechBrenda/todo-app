6d65b1f1196f99904df40977896fff5304440288
TechBrenda <tech.brenda@gmail.com>
2019-07-01 10:07:02 -0500

    Removed local methods from CounterButton

--- todo-app/src/components/counter/CounterButton.js
+++ todo-app/src/components/counter/CounterButton.js
@@ -3,19 +3,11 @@ import PropTypes from 'prop-types'
 import './Counter.css'
 
 class CounterButton extends Component {
-  increment = () => {
-    this.props.incrementMethod(this.props.by)
-  }
-
-  decrement = () => {
-    this.props.incrementMethod(-this.props.by)
-  }
-
   render () {
     return (
       <div className='counter'>
-        <button onClick={this.increment}>+{this.props.by}</button>
-        <button onClick={this.decrement}>-{this.props.by}</button>
+        <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+        <button onClick={() => this.props.incrementMethod(-this.props.by)}>-{this.props.by}</button>
       </div>
     )
   }
