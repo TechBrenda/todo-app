7ad48c241f7945e86140f239e3a23d942aeefd91
TechBrenda <tech.brenda@gmail.com>
2019-07-01 14:30:24 -0500

    Added handler functions for form inputs

--- todo-app/src/components/todo/Login.js
+++ todo-app/src/components/todo/Login.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: 'techbrenda',
+      password: ''
+    }
+  }
+
+  handleUsernameChange = event => {
+    this.setState({ username: event.target.value })
+  }
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value })
+  }
+
   render () {
     return (
       <div>
         <span>User Name:</span>
-        <input type='text' name='username' />
+        <input
+          type='text'
+          name='username'
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
         <span>Password:</span>
-        <input type='password' name='password' />
+        <input
+          type='password'
+          name='password'
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
         <button>Login</button>
       </div>
     )
