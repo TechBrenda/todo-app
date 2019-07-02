792f3a6b4b8e090081fcebc92620d9cc601efa39
TechBrenda <tech.brenda@gmail.com>
2019-07-02 14:55:01 -0500

    Scaffold navigation

--- todo-app/src/components/todo/Header.js
+++ todo-app/src/components/todo/Header.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   render () {
     return (
-      <div>
-        Header
-        <hr />
-      </div>
+      <header>
+        <nav>
+          <div>
+            <Link to='/'>Home</Link>
+          </div>
+          <ul>
+            <li>welcome</li>
+            <li>todos</li>
+          </ul>
+          <ul>
+            <li>profile</li>
+            <li>logout</li>
+          </ul>
+        </nav>
+      </header>
     )
   }
 }
