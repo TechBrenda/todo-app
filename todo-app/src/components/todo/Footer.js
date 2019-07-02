cbec6990c2235d1eb6a48bc9a9eb4247626c6f56
TechBrenda <tech.brenda@gmail.com>
2019-07-02 15:49:53 -0500

    Styled header and footer

--- todo-app/src/components/todo/Footer.js
+++ todo-app/src/components/todo/Footer.js
@@ -3,10 +3,9 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render () {
     return (
-      <div>
-        Footer
-        <hr />
-      </div>
+      <footer className='footer'>
+        <span className='text-muted'>All Rights Reserved 2019 @techbrenda</span>
+      </footer>
     )
   }
 }
--- todo-app/src/components/todo/Header.js
+++ todo-app/src/components/todo/Header.js
@@ -5,17 +5,17 @@ class Header extends Component {
   render () {
     return (
       <header>
-        <nav>
+        <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
           <div>
-            <Link to='/'>Home</Link>
+            <Link to='/' className='navbar-brand'>Site</Link>
           </div>
-          <ul>
-            <li>welcome</li>
-            <li>todos</li>
+          <ul className='navbar-nav'>
+            <li><Link className='nav-link' to='/welcome/techbrenda'>Home</Link></li>
+            <li><Link className='nav-link' to='/todo'>Todos</Link></li>
           </ul>
-          <ul>
-            <li>profile</li>
-            <li>logout</li>
+          <ul className='navbar-nav navbar-collapse justify-content-end'>
+            <li><Link className='nav-link' to='/login'>Login</Link></li>
+            <li><Link className='nav-link' to='/logout'>Logout</Link></li>
           </ul>
         </nav>
       </header>
