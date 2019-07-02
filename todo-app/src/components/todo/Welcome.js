84439eabd7acc3a484e7db9dfe9dc958ab51a1c2
TechBrenda <tech.brenda@gmail.com>
2019-07-02 13:34:54 -0500

    Added link to TodoList

--- todo-app/src/components/todo/Welcome.js
+++ todo-app/src/components/todo/Welcome.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import TodoList from './TodoList'
+import { Link } from 'react-router-dom'
 
 class Welcome extends Component {
   render () {
     return (
       <div>
-        <h2>Welcome {this.props.match.params.name}</h2>
-        <TodoList />
+        Welcome {this.props.match.params.name}.<br />
+        <Link to='/todo'>Manage your Todos</Link>
       </div>
     )
   }
