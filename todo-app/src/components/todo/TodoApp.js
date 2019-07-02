c973d2866b5ca25fd1292c5499f14b37f9464da4
TechBrenda <tech.brenda@gmail.com>
2019-07-02 08:51:52 -0500

    Added routing

--- todo-app/src/components/todo/TodoApp.js
+++ todo-app/src/components/todo/TodoApp.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './Login'
 import './TodoApp.css'
+import Welcome from './Welcome'
 
 class TodoApp extends Component {
   render () {
     return (
       <div className='TodoApp'>
-        <Login />
+        <Router>
+          <Route path='/' exact component={Login} />
+          <Route path='/login' component={Login} />
+          <Route path='/welcome' component={Welcome} />
+        </Router>
       </div>
     )
   }
