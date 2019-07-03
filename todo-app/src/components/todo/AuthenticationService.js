4fd3f2e49e6cd773b5db2a8feb0bc0b93d3e7146
TechBrenda <tech.brenda@gmail.com>
2019-07-03 11:28:22 -0500

    Remove username from sessionStorage on logout

--- todo-app/src/components/todo/AuthenticationService.js
+++ todo-app/src/components/todo/AuthenticationService.js
@@ -2,6 +2,10 @@ class AuthenticationService {
   registerSuccessfulLogin (username, password) {
     window.sessionStorage.setItem('authenticatedUser', username)
   }
+
+  logout () {
+    window.sessionStorage.removeItem('authenticatedUser')
+  }
 }
 
 export default new AuthenticationService()
