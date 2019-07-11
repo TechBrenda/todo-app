da055703a1467f348997f369ade935557d86bbb5
TechBrenda <tech.brenda@gmail.com>
2019-07-11 16:58:31 -0500

    Added Delete controller to Todo

--- restful-web-services/src/main/java/com/techbrenda/rest/webservices/restfulwebservices/todo/Todo.java
+++ restful-web-services/src/main/java/com/techbrenda/rest/webservices/restfulwebservices/todo/Todo.java
@@ -3,13 +3,16 @@ package com.techbrenda.rest.webservices.restfulwebservices.todo;
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Todo {
+  @EqualsAndHashCode.Include
   private long id;
   private String username;
   private String description;
--- restful-web-services/src/main/java/com/techbrenda/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ restful-web-services/src/main/java/com/techbrenda/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -16,8 +16,28 @@ public class TodoHardcodedService {
     todos.add(new Todo(idCounter++, "techbrenda", "Learn about Microservices", new Date(), false));
     todos.add(new Todo(idCounter++, "techbrenda", "Learn about Redux", new Date(), false));
   }
-  
+
   public List<Todo> findAll() {
     return todos;
+  }
+
+  public Todo findById(long id) {
+    for (Todo todo : todos) {
+      if (todo.getId() == id) {
+        return todo;
+      }
+    }
+    return null;
+  }
+
+  public Todo deleteById(long id) {
+    Todo todo = findById(id);
+    if (todo == null) {
+      return null;
+    }
+    if (todos.remove(todo)) {
+      return todo;
+    }
+    return null;
   }
 }
