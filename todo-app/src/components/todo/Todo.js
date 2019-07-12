3de70813db1af2a5bc16ae529b7fbb5ab3653338
TechBrenda <tech.brenda@gmail.com>
2019-07-11 21:22:25 -0500

    Added initial state

--- todo-app/src/components/todo/Todo.js
+++ todo-app/src/components/todo/Todo.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 class Todo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      id: -1,
+      description: '',
+      targetDate: new Date()
+    }
+  }
+
   render () {
     return <div>Todo: {this.props.match.params.id}</div>
   }
