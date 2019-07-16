1151152d01564ae7367409731dabf63d072af868
TechBrenda <tech.brenda@gmail.com>
2019-07-16 13:12:29 -0500

    Created form handler function and initial values

--- todo-app/src/components/todo/Todo.js
+++ todo-app/src/components/todo/Todo.js
@@ -7,29 +7,49 @@ class Todo extends React.Component {
     super(props)
     this.state = {
       id: this.props.match.params.id,
-      description: '',
+      description: 'Learn Forms Now',
       targetDate: moment().format('YYYY-MM-DD')
     }
   }
 
+  handleSubmit = values => {
+    console.log(values)
+  }
+
   render () {
+    let { description, targetDate } = this.state
     return (
       <div>
         <h1>Todo</h1>
         <div className='container'>
-          <Formik>{(props) => (
-            <Form>
-              <fieldset className='form-group'>
-                <label>Description</label>
-                  <Field className='form-control' type='text' name='description' />
-              </fieldset>
-              <fieldset className='form-group'>
-                <label>Target Date</label>
-                  <Field className='form-control' type='date' name='targetDate' />
-              </fieldset>
-              <button className='btn btn-success' type='submit'>Save</button>
-            </Form>
-          )}</Formik>
+          <Formik
+            initialValues={{ description, targetDate }}
+            onSubmit={this.handleSubmit}
+          >
+            {props => (
+              <Form>
+                <fieldset className='form-group'>
+                  <label>Description</label>
+                  <Field
+                    className='form-control'
+                    type='text'
+                    name='description'
+                  />
+                </fieldset>
+                <fieldset className='form-group'>
+                  <label>Target Date</label>
+                  <Field
+                    className='form-control'
+                    type='date'
+                    name='targetDate'
+                  />
+                </fieldset>
+                <button className='btn btn-success' type='submit'>
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     )
