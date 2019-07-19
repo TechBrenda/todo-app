2a9a4300575d3e76fcd45deda0333b9ec2cc1928
TechBrenda <tech.brenda@gmail.com>
2019-07-19 15:03:34 -0500

    JWT Code from course

---
+++ restful-web-services/src/main/java/com/techbrenda/rest/webservices/restfulwebservices/jwt/resource/JwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package com.techbrenda.rest.webservices.restfulwebservices.jwt.resource;
+
+public class JwtAuthenticationException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public JwtAuthenticationException(String message, Throwable cause) {
+      super(message, cause);
+  }
+}
