2efacdbbbf547c6e09639a1753767f07d941aa98
TechBrenda <tech.brenda@gmail.com>
2019-07-24 15:46:39 -0500

    Added insert statements for H2

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001, 'techbrenda', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002, 'techbrenda', 'Learn H2', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003, 'techbrenda', 'Learn Spring Boot Unit Testing', sysdate(), false);
